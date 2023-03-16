14f54f9989282bf48495d564d3c6b306b6ab070c
Noemi <noemi.caggiano@gmail.com>
2023-03-16 16:34:37 +0000

    feat: create basic component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
-import React from 'react';
+import React, {FC} from 'react';
+import { Person } from './components/Person';
 import './App.css';
 
-function App() {
-
-
-
+const App: FC = () => {
   return (
     <div className="App">
-      
+      <Person />
     </div>
   );
 }
