2632d49223d7665f7687afcb55ee3825d8c97b03
Noemi <noemi.caggiano@gmail.com>
2023-03-17 09:49:08 +0000

    feat: state management & context

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,34 @@
-import React, {FC} from 'react';
+import React, {FC, createContext} from 'react';
 import { Person } from './components/Person';
+import { HairColor } from './components/Person';
 import './App.css';
 
+
+interface AppContextInterface {
+  name: string;
+  age: number;
+  country: string;
+}
+
+const AppContext = createContext<AppContextInterface | null>(null)
+
+const contextValue: AppContextInterface = {
+  name: 'Pedro',
+  age: 20,
+  country: 'Brazil'
+}
+
 const App: FC = () => {
+
+
   return (
+    <AppContext.Provider value={contextValue}>
     <div className="App">
-      <Person name="Noemi" age={20} email="noemiemail@gmail.com"/>
+      <Person name="Noemi" age={20} email="noemiemail@gmail.com" hairColor={HairColor.Brown}/>
     </div>
+    </AppContext.Provider>
   );
 }
 
 export default App;
+
--- src/components/Person.tsx
+++ src/components/Person.tsx
@@ -1,18 +1,36 @@
 
-import {FC} from 'react';
+import {FC, ChangeEvent, useState} from 'react';
+
+export enum HairColor {
+  Blonde= 'your hair is blonde, good for you!',
+  Brown= 'Cool hair color!',
+  Pink= 'Wow! That is so cool!'
+}
 
 interface Props {
   name: string,
   age: number,
-  email: string
+  email: string,
+  hairColor: HairColor
 }
 
-export const Person: FC<Props> = ({name, age, email}) => {
+export const Person: FC<Props> = ({name, age, email, hairColor}) => {
+
+  const [country, setCountry] = useState<string | null>(null)
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setCountry(e.target.value)
+  }
+
   return (
     <div>
       <h1>{name}</h1>
       <h1>{email}</h1>
       <h1>{age}</h1>
+      <input placeholder='Write down your country..' onChange={onChange}/>
+
+      {country}
+      {hairColor}
     </div>
   );
 }
